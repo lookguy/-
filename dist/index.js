af5e993a3514ed3c8fabfe5ba2c9fccaea86337d
zxb <614619114@qq.com>
2019-04-09 11:34:06 +0800

    折腾了下babel

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _stack = require("./stack");
+
+const stack = new _stack.Stack();
+console.log(stack.isEmpty());
---
+++ dist/stack.js
@@ -0,0 +1,56 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Stack = void 0;
+
+require("core-js/modules/es6.regexp.to-string");
+
+class Stack {
+  //选用议中数据结构来保存栈里的元素
+  constructor() {
+    this.items = [];
+  } //添加元素
+
+
+  push(el) {
+    this.items.push(el);
+  } //移除元素
+
+
+  pop(el) {
+    this.items.pop();
+  } //返回栈顶的元素
+
+
+  peek() {
+    return this.items[items.length - 1];
+  } //判断栈里是否有元素
+
+
+  isEmpty() {
+    return this.items.length === 0;
+  } //移除栈里所有元素
+
+
+  clear() {
+    for (let i = 0; i < this.items.length; i++) {
+      this.items.pop();
+    }
+  } //返回栈里元素个数
+
+
+  size() {
+    return this.items.length;
+  } //打印
+
+
+  print() {
+    console.log(this.items.toString());
+    return false;
+  }
+
+}
+
+exports.Stack = Stack;
